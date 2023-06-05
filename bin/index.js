03f68411bf190551a5dd3ba3c13385cedd1f134e
JustGitHubUserid9292 <118734378+JustGitHubUserid9292@users.noreply.github.com>
2023-06-05 13:35:23 +0300

    finally first fucking game

--- bin/index.js
+++ bin/index.js
@@ -2,89 +2,91 @@
 
 import readlineSync from 'readline-sync'
 
+import inquirer from "inquirer";
+
 console.log("Welcome to Rock Paper Scissors!");
 console.log('In this game, you have to beat the computer in three rounds.');
-
-let isPlaying = true;
-let playerScore = 0;
-let computerScore = 0;
-
-const getPlayerMove = () => {
-  const move = readlineSync.question('Your move: ');
-  if (move === null) {
-    isPlaying = false;
-  }
-  return move;
-};
-
-const getComputerMove = () => {
-  const moves = ['rock', 'scissors', 'paper'];
-  const randomIndex = Math.floor(Math.random() * moves.length);
-  return moves[randomIndex];
-};
-
-const determineRoundResult = (playerMove, computerMove) => {
-  if (playerMove === computerMove) {
-    return 'tie';
-  } else if (
-    (playerMove === 'rock' && computerMove === 'scissors') ||
-    (playerMove === 'scissors' && computerMove === 'paper') ||
-    (playerMove === 'paper' && computerMove === 'rock')
-  ) {
-    return 'win';
-  } else {
-    return 'lose';
-  }
-};
-
-const displayRoundResult = (result) => {
-  if (result === 'tie') {
-    console.log('Once again!');
-  } else if (result === 'win') {
-    playerScore += 1;
-    console.log(`You win ${playerScore} round(s)!`);
-  } else {
-    computerScore += 1;
-    console.log(`Computer wins ${computerScore} round(s)!`);
-  }
-};
-
-const displayFinalResult = () => {
-  if (playerScore === 3) {
-    console.log(`Congratulations, ${name}!`);
-  } else {
-    console.log('Computer wins!');
-  }
-  console.log(`Score: ${playerScore}:${computerScore}`);
-};
-
-const startNewGame = () => {
-  console.clear(); 
-  playerScore = 0;
-  computerScore = 0;
-};
 
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
 
-while (isPlaying) {
-  const playerMove = getPlayerMove();
-  if (!isPlaying) {
-    break;
-  }
-  const computerMove = getComputerMove();
-  console.log(`Opponent's move: ${computerMove}`);
-  const roundResult = determineRoundResult(playerMove, computerMove);
-  displayRoundResult(roundResult);
+let playerScore = 0;
+let computerScore = 0;
 
-  if (playerScore === 3 || computerScore === 3) {
-    displayFinalResult();
-    const playAgain = readlineSync.question("Do you want to play again? (yes/no): ");
-    if (playAgain.toLowerCase() !== 'yes') {
-      isPlaying = false;
-      console.clear()
-    } else {
-      startNewGame()
-    }
+playGame();
+
+function playGame() {
+  if (playerScore < 3 && computerScore < 3) {
+    inquirer
+      .prompt([
+        {
+          type: 'list',
+          name: 'userChoice',
+          message: 'Your move: ',
+          choices: ['rock', 'scissors', 'paper'],
+        },
+      ])
+      .then((answers) => {
+        const userChoice = answers.userChoice;
+        const computerChoice = generateComputerChoice();
+
+        console.log("Opponent's move:", computerChoice);
+        displayRoundResult(determineWinner(userChoice, computerChoice));
+
+        playGame(); 
+      });
+  } else {
+    displayFinalResult()
+    if (readlineSync.keyInYNStrict("Do you want to play again?")) {
+        startNewGame();
+    } else console.clear()
   }
+  }
+
+function generateComputerChoice() {
+    const moves = ['rock', 'scissors', 'paper'];
+    const randomIndex = Math.floor(Math.random() * moves.length);
+    return moves[randomIndex];
+}
+
+function determineWinner(userChoice, computerChoice) {
+    if (userChoice === computerChoice) {
+        return 'tie';
+      } else if (
+        (userChoice === 'rock' && computerChoice === 'scissors') ||
+        (userChoice === 'scissors' && computerChoice === 'paper') ||
+        (userChoice === 'paper' && computerChoice === 'rock')
+      ) {
+        return 'win';
+      } else {
+        return 'lose';
+    }
+}
+
+function displayRoundResult(result) {
+    if (result === 'tie') {
+      console.log('Once again!');
+    } else if (result === 'win') {
+      playerScore += 1;
+      console.log(`You win ${playerScore} round(s)!`);
+    } else {
+      computerScore += 1;
+      console.log(`Computer wins ${computerScore} round(s)!`);
+    }
+};
+
+const displayFinalResult = () => {
+    if (playerScore === 3) {
+      console.log(`Congratulations, ${name}!`);
+    } else {
+      console.log('Computer wins!');
+    }
+    console.log(`Score: ${playerScore}:${computerScore}`);
+  };
+
+function startNewGame() {
+    console.clear();
+    playerScore = 0;
+    computerScore = 0;
+    playGame();
 }
