94a5f87feaa129962fcb3a966590579376353195
JustGitHubUserid9292 <118734378+JustGitHubUserid9292@users.noreply.github.com>
2023-06-27 22:08:19 +0300

    Update index.js

--- bin/index.js
+++ bin/index.js
@@ -6,61 +6,11 @@
 /* eslint-disable import/no-cycle */
 import readlineSync from 'readline-sync';
 
-import inquirer from 'inquirer';
-
-import rps from '../games/rps.js';
-
-import words from '../games/words.js';
-
-import slotmachine from '../games/slotmachine.js';
-
-import shootthedice from '../games/shootthedice.js';
-
-import hangman from '../games/hangman.js';
+import mainMenu from '../games/mainMenu.js';
 
 console.clear();
 console.log('Welcome to Game Center!');
-const name = readlineSync.question('May I have your name? ');
-console.log(`Hello, ${name}!`);
+export const user = readlineSync.question('May I have your name? ');
+console.log(`Hello, ${user}!`);
 
-mainMenu();
-
-export default function mainMenu() {
-  inquirer
-    .prompt([
-      {
-        type: 'list',
-        name: 'userChoice',
-        message: 'Choose the game',
-        choices: ['Rock Paper Scissors', 'The Words Game', 'Slot Machine', 'Shoot The Dice', 'Hangman', 'Exit'],
-      },
-    ])
-    .then((answers) => {
-      const userChoice = answers.userChoice;
-      if (userChoice === 'Rock Paper Scissors') {
-        console.clear();
-        rps(name);
-      }
-      if (userChoice === 'The Words Game') {
-        console.clear();
-        words(name);
-      }
-      if (userChoice === 'Slot Machine') {
-        console.clear();
-        slotmachine(name);
-      }
-      if (userChoice === 'Shoot The Dice') {
-        console.clear();
-        shootthedice(name);
-      }
-      if (userChoice === 'Hangman') {
-        console.clear();
-        hangman(name);
-      }
-      if (userChoice === 'Exit') {
-        console.clear();
-        console.log(' ');
-        console.log(`See you next time, ${name}!`);
-      }
-    });
-}
+mainMenu(user);
